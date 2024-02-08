9ef94a03a8f1fc4b9856643fb4f968dff1809342
Tino Oasis Laptop <falentinodjoka2801@gmail.com>
2024-02-08 23:24:31 +0700

    Pengembangan MitraFilter: Penambahan pengecekan JWT

--- app/Filters/MitraFilter.php
+++ app/Filters/MitraFilter.php
@@ -5,19 +5,44 @@
     use CodeIgniter\HTTP\RequestInterface;
     use CodeIgniter\HTTP\ResponseInterface;
 
+    #Model
     use App\Models\Mitra;
+
+    #Library
+    use App\Libraries\MitraJWT;
 
     class MitraFilter implements FilterInterface{
         public function before(RequestInterface $request, $arguments = null){
             $session        =   session();
             $mitraSession   =   $session->get('mitra');
             
+            $mitra          =   new Mitra();
+            $mitraJWT       =   new MitraJWT();
+
+            $authorization          =   $request->getServer('HTTP_AUTHORIZATION');
+            if(!empty($authorization)){
+                $encodedJWT             =   null;  
+                if (preg_match('/Bearer\s(\S+)/', $authorization, $matches)) {
+                    $encodedJWT =   $matches[1];
+                }        
+
+                if(!empty($encodedJWT)){
+                    $decodedAuthorization   =   $mitraJWT->decode($encodedJWT);
+                    $detailMitra            =   (property_exists($decodedAuthorization, 'mitra'))? $decodedAuthorization->mitra : null;
+                    if(!empty($detailMitra)){
+                        $idMitra            =   $detailMitra->id;
+                        $detailMitra        =   $mitra->getMitra($idMitra);
+
+                        $request->mitra     =   $detailMitra;
+                        return $request;
+                    }
+                }
+            }
+
             if(empty($mitraSession)){
                 $request->mitra     =   null;
                 return redirect()->to(mitraController('login'));
             }
-            
-            $mitra          =   new Mitra();
 
             $id             =   $mitraSession['id'];
             $detailMitra    =   $mitra->getMitra($id);
