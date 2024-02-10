4b58aa0dea8a8cce23422b39697e4878b7d2f8e0
thomas <thomas@mail.com>
2024-02-10 12:49:45 +0700

    test

--- app/Config/App.php
+++ app/Config/App.php
@@ -6,7 +6,7 @@ use CodeIgniter\Config\BaseConfig;
 
 class App extends BaseConfig
 {
-    public $appName         =   'Employer Kubu.id';
+    public $appName         =   'Employer Kubu ID';
     public $appDescription  =   'Website Lowongan Pekerjaan';
 
     public $appNickName     =   'employer';
