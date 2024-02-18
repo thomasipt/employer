1384e22d5d3b9066017725ac6f6d1b95576d899c
Tino Oasis Laptop <falentinodjoka2801@gmail.com>
2024-02-18 14:08:19 +0700

    Penyelesaian Registrasi Mitra

--- app/Controllers/website/Home.php
+++ app/Controllers/website/Home.php
@@ -2,12 +2,17 @@
     namespace App\Controllers\website;
     
     use App\Controllers\BaseController;
-
+    
     #Library
     use App\Libraries\FormValidation;
-
+    use App\Libraries\Tabel;
+    use App\Libraries\APIRespondFormat;
+    use App\Libraries\EmailSender;
+    use App\Libraries\ErrorCode;
+    
     #Models
     use App\Models\KategoriLoker;
+    use App\Models\Mitra;
 
     use CodeIgniter\API\ResponseTrait;
 
@@ -32,7 +37,9 @@
         }
         public function registrasi(){
             $kategoriLoker  =   new KategoriLoker();
-            
+            $error          =   new ErrorCode();
+            $emailSender    =   new EmailSender();
+
             $options        =   [
                 'select'    =>  'sektor_id as id, name as nama',
                 'order_by'  =>  [
@@ -45,6 +52,10 @@
             $pageData   =   [
                 'pageTitle' =>  'Registrasi',
                 'view'      =>  websiteView('registrasi'),
+                'library'   =>  [
+                    'error'         =>  $error,
+                    'emailSender'   =>  $emailSender
+                ],
                 'data'      =>  [
                     'listSektor'    =>  $listSektor
                 ]
@@ -52,18 +63,120 @@
             return view(websiteView('index'), $pageData);
         }
         public function prosesRegistrasi(){
+            $status     =   false;
+            $message    =   'Gagal memproses registrasi mitra! Silahkan ulangi!';
+            $data       =   null;
+            $code       =   null;
+
             $formValidation     =   new FormValidation();
+            $tabel              =   new Tabel();
+            $emailSender        =   new EmailSender();
+            $mitraModel         =   new Mitra();
+
+            $db     =   $mitraModel->db;
+
+            $db->transBegin();
 
             try{
+                $configComplete     =   $emailSender->isConfigComplete();
+                if(!$configComplete){
+                    throw new Exception('Harap hubungi administrator untuk menyelesaikan konfigurasi email');
+                }
+
                 $dataMitra          =   [];
                 $validationRules    =   [
                     'nama'      =>  $formValidation->rule_required,
                     'alamat'    =>  $formValidation->rule_required,
-                    'telepon'   =>  $formValidation->rule_required.'|'.$formValidation->rule_numeric
+                    'telepon'   =>  $formValidation->rule_required.'|'.$formValidation->rule_numeric.'|is_unique['.$tabel->mitra.'.telepon]',
+                    'email'     =>  $formValidation->rule_validEmail.'|'.$formValidation->rule_required.'|is_unique['.$tabel->mitra.'.email]',
+                    'sektor'    =>  $formValidation->rule_required,
+                    'pic'       =>  $formValidation->rule_required
                 ];
-            }catch(Exception $e){
+                $validationMessages     =   $formValidation->generateCustomMessageForSingleRule($validationRules);
 
+                $message    =   'Data tidak valid!';
+                if($this->validate($validationRules, $validationMessages)){
+                    $request    =   request();
+
+                    $nama       =   $request->getPost('nama');
+                    $alamat     =   $request->getPost('alamat');
+                    $telepon    =   $request->getPost('telepon');
+                    $email      =   $request->getPost('email');
+                    $sektor     =   $request->getPost('sektor');
+                    $pic        =   $request->getPost('pic');
+
+                    $sektorModel    =   new KategoriLoker();
+                    $detailSektor   =   $sektorModel->getKategoriLoker($sektor);
+                    if(empty($detailSektor)){
+                        throw new Exception('Sektor pekerjaan tidak terdaftar!');
+                    }
+
+                    $passwordMitra  =   '123456';
+
+                    $dataMitra  =   [
+                        'nama'      =>  $nama,
+                        'alamat'    =>  $alamat,
+                        'telepon'   =>  $telepon,
+                        'pic_name'  =>  $pic,
+                        'email'     =>  $email,
+                        'sector'    =>  $sektor,
+                        'password'  =>  md5($passwordMitra)
+                    ];
+                    $saveMitra  =   $mitraModel->saveMitra(null, $dataMitra);
+                    
+                    $message    =   'Gagal memproses pendaftaran mitra! Silahkan ulangi!';
+                    if($saveMitra){
+                        $idMitraBaru    =   $saveMitra;
+
+                        $encodedIDMitra =   base64_encode($idMitraBaru);
+                        $linkVerifikasi =   site_url(websiteController('verifikasi/'.$encodedIDMitra));
+
+                        $status     =   true;
+                        $message    =   'Pendaftaran berhasil!';
+                        $data       =   ['id' => $idMitraBaru];
+
+                        $htmlBody   =   '<div style="width: 100%; border: 1px solid #0D6EFD; border-radius: 10px; padding: 15px;">
+                                            <center>
+                                                <img src="https://employer.kubu.id/assets/img/icon.png" style="width: 150px; display: block; margin: auto;"
+                                                    alt="Employer" />
+                                            </center>
+                                            <p>Anda telah mendaftar sebagai mitra ke dalam website <a href="https://employer.kubu.id">Employer</a>.
+                                            Silahkan klik link berikut ini untuk memverifikasi pendaftaran anda <a href="'.$linkVerifikasi.'">'.$linkVerifikasi.'</a></p>
+                                            <p>Selanjutnya anda hanya perlu menunggu approvement dari Administrator Employer untuk mengaktifkan akun anda.</p>
+                                            <p style="color: red;">Jika anda tidak melakukan pendaftaran, silahkan abaikan pesan ini</p>
+                                        </div>';
+
+                        $emailParams    =   [
+                            'subject'   =>  'Pendaftaran Mitra Employer',
+                            'body'      =>  $htmlBody,
+                            'receivers' =>  [
+                                ['email' => $email, 'name' => $nama]
+                            ]
+                        ];
+                        $sendEmail          =   $emailSender->sendEmail($emailParams);   
+                        $statusKirimEmail   =   $sendEmail['statusSend'];
+                        if($statusKirimEmail){
+                            $db->transCommit();
+                        }else{
+                            $db->transRollback();
+                        }
+                    }
+                }else{
+                    $error  =   new ErrorCode();
+
+                    $code   =   $error->formValidationError;
+                    $data   =   $this->validator->getErrors();
+                }
+            }catch(Exception $e){
+                $status     =   false;
+                $message    =   $e->getMessage();
+                $data       =   null;
             }
+
+            $arf        =   new APIRespondFormat($status, $message, $data, $code);
+            $respond    =   $arf->getRespond();
+
+            return $this->respond($respond);
         }
     }
 ?>
--- app/Views/website/registrasi.php
+++ app/Views/website/registrasi.php
@@ -1,40 +1,48 @@
 <?php
-    $listSektor =   $data['listSektor'];
+    $listSektor     =   $data['listSektor'];
+    $errorLib       =   $library['error'];
+    $emailSender    =   $library['emailSender'];
+
+    $configComplete     =   $emailSender->isConfigComplete();
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-lg-4">
-            <div class="p-3" style='background-color: #ecf3ff; border-radius: 10px;'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere debitis modi officia nobis dolorem. Excepturi aliquam iste qui labore numquam! Exercitationem ipsam odio rem recusandae laboriosam voluptas, sit veniam?
-            </div>
+        <div class="col-lg-5 d-flex align-items-center px-5">
+            <img src="<?=base_url(assetsFolder('flexstart/assets/img/features-3.png'))?>" class='w-100 d-block m-auto' alt="Pendaftaran Mitra" />
         </div>
-        <div class="col-lg-8" id='formContainer'>
+        <div class="col-lg-7" id='formContainer'>
             <h5><b>Form Registrasi</b></h5>
             <br />
-            <form action="<?=site_url(mitraController('registrasi'))?>" id="formMitra">
+            <?php if(!$configComplete){ ?>
+                <div class="alert alert-warning">
+                    Harap hubungi administrator untuk melengkapi konfigurasi email, karena konfigurasi tersebut digunakan untuk pengiriman email verifikasi pendaftaran mitra anda
+                </div>
+                <br />
+            <?php } ?>
+            <form action="<?=site_url(websiteController('process-registrasi'))?>" id="formMitra">
                 <div class="form-group mb-3">
                     <label for="nama" class='d-block mb-2'>Nama</label>
                     <input type="text" id="nama" class="form-control" name='nama'
-                        placeholder='Nama Lengkap Perusahaan' required />
+                        placeholder='Nama Lengkap Perusahaan' required <?=(!$configComplete)? 'disabled' : ''?> />
                 </div>
                 <div class="form-group mb-3">
                     <label for="alamat" class='d-block mb-2'>Alamat</label>
                     <textarea id="alamat" class="form-control" name='alamat'
-                        placeholder='Alamat Lengkap Perusahaan' required></textarea>
+                        placeholder='Alamat Lengkap Perusahaan' required <?=(!$configComplete)? 'disabled' : ''?>></textarea>
                 </div>
                 <div class="form-group mb-3">
                     <label for="telepon" class='d-block mb-2'>Telepon</label>
                     <input type="number" id="telepon" class="form-control" name='telepon'
-                        placeholder='Nomor Telepon' required />
+                        placeholder='Nomor Telepon' required <?=(!$configComplete)? 'disabled' : ''?> />
                 </div>
                 <div class="form-group mb-3">
                     <label for="email" class='d-block mb-2'>Email</label>
                     <input type="email" id="email" class="form-control" name='email'
-                        placeholder='Alamat Email Resmi' required />
+                        placeholder='Alamat Email Resmi' required <?=(!$configComplete)? 'disabled' : ''?> />
                 </div>
                 <div class="form-group mb-3">
                     <label for="sektor" class='d-block mb-2'>Sektor</label>
-                    <select name='sektor' class='form-control' id='sektor'>
+                    <select name='sektor' class='form-control' id='sektor' required <?=(!$configComplete)? 'disabled' : ''?>>
                         <option value="">-- Pilih Sektor --</option>
                         <?php foreach($listSektor as $sektor){ ?>
                             <?php
@@ -48,12 +56,14 @@
                 <div class="form-group mb-3">
                     <label for="pic" class='d-block mb-2'>Nama Penanggung Jawab</label>
                     <input type="text" id="pic" class="form-control" name='pic'
-                        placeholder='Nama Lengkap Penanggung Jawab' required />
+                        placeholder='Nama Lengkap Penanggung Jawab' required <?=(!$configComplete)? 'disabled' : ''?> />
                 </div>
-                <hr class='mb-4' />
-                <button class="btn btn-success" type='submit' id='btnSubmit'>
-                    Daftar sebagai Mitra
-                </button>
+                <?php if($configComplete){ ?>
+                    <hr class='mb-4' />
+                    <button class="btn btn-success" type='submit' id='btnSubmit'>
+                        Daftar sebagai Mitra
+                    </button>
+                <?php } ?>
             </form>
         </div>
     </div>
@@ -61,14 +71,18 @@
 <script src="<?= base_url(assetsFolder('plugins/jquery/jquery.min.js')) ?>/"></script>
 <link rel="stylesheet" href="<?=base_url(assetsFolder('plugins/ionicons/ionicons.min.css'))?>" />
 
+<script src='<?= base_url(assetsFolder('plugins/sweetalert2/sweetalert2.min.js')) ?>'></script>
+<link rel="stylesheet" href="<?=base_url(assetsFolder('plugins/sweetalert2/sweetalert2.min.css'))?>" />
+
 <script src='<?=base_url(assetsFolder('custom/js/form-submission.js'))?>'></script>
+<script src='<?=base_url(assetsFolder('custom/js/custom-alert.js'))?>'></script>
 
 <script src='<?=base_url(assetsFolder('plugins/select2/js/select2.min.js'))?>'></script>
 <link rel="stylesheet" href="<?=base_url(assetsFolder('plugins/select2/css/select2.min.css'))?>" />
 <link rel="stylesheet" href="<?=base_url(assetsFolder('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css'))?>" />
 
 <script language='Javascript'>
-    let _formMitra  =   $('#formMitra');
+    let _formMitra      =   $('#formMitra');
 
     $('#sektor').select2({
         theme: 'bootstrap4'
@@ -77,7 +91,43 @@
     _formMitra.on('submit', async function(e){
         e.preventDefault();
         await submitForm(this, async function(responseFromServer){
-            console.log(responseFromServer);
+            let _formValidationErrorCode    =   `<?=$errorLib->formValidationError?>`;
+
+            let _status     =   responseFromServer.status;
+            let _code       =   responseFromServer.code;
+            let _message    =   responseFromServer.message;
+            let _data       =   responseFromServer.data;
+            
+            let _swalTitle      =   'Pendaftaran Mitra';
+            let _swalMessage    =   (_message == null)? (_status)? 'Berhasil!' : 'Gagal!' : _message;
+            let _swalType       =   (_status)? 'success' : 'error';
+            
+            await notifikasi(_swalTitle, _swalMessage, _swalType);
+
+            if(_status){
+                location.href   =   `<?=site_url()?>`;
+            }else{
+                if(_code == _formValidationErrorCode){
+                    //Reset Form Validation
+                    let _formControlElement     =   _formMitra.find('.form-control');
+                    _formControlElement.removeClass('border-danger');
+                    _formControlElement.next().remove();
+
+                    //Memunculkan error
+                    $.each(_data, function(formName, formError){
+                        let _formElement    =   `[name=${formName}]`;
+                        
+                        // $(_formElement).removeClass('border-danger');
+                        $(_formElement).addClass('border-danger');
+
+                        let _nextElement    =   $(_formElement).next();
+                        if(_nextElement.length >= 1){
+                            _nextElement.remove();
+                        }
+                        $(_formElement).after(`<p class='text-sm mt-2 text-danger'>${formError}</p>`);
+                    });
+                }
+            }
         });
     });
 </script>
