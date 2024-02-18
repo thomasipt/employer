c6aef3a2ab2aaea02d9fa8a79448d6d46be4e575
Tino Oasis Laptop <falentinodjoka2801@gmail.com>
2024-02-18 13:01:00 +0700

    Pengembangan APIResponseFormat: Penambahan parameter code

--- app/Libraries/APIRespondFormat.php
+++ app/Libraries/APIRespondFormat.php
@@ -4,9 +4,10 @@
     class APIRespondFormat{
         protected $respond;
 
-        public function __construct($status, $message, $data){
+        public function __construct($status, $message, $data, $code = null){
             $returnedData   =   [
                 'status'    =>  $status,
+                'code'      =>  $code,
                 'message'   =>  $message,
                 'data'      =>  $data
             ];
