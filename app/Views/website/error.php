1913bb0c7e15fd5609da71508823ccb4f50c860b
Tino Oasis Laptop <falentinodjoka2801@gmail.com>
2024-02-22 00:20:38 +0700

    Halaman baru: Error Website

---
+++ app/Views/website/error.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col text-center">
+            <h4><?=$title?></h4>
+            <p class="text-sm text-muted"><?=$deskripsi?></p>
+        </div>
+    </div>
+</div>
