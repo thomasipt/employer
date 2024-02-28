5b161606b00b3de40b6bcfbf8c659315fc730e29
thomasipt <thomas_ipt2007@yahoo.com>
2024-02-28 10:31:09 +0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -12,7 +12,7 @@ class App extends BaseConfig
     public $appNickName     =   'employer';
     public $emailAccountUsername; #email address
     public $emailAccountPassword; #sandi aplikasi (bukan sandi email)
-    public string $baseURL = 'http://localhost/employer/';
+    public string $baseURL = 'https://employer.kubu.id/';
     public array $allowedHostnames = [];
     public string $indexPage = '';
     public string $uriProtocol = 'REQUEST_URI';
