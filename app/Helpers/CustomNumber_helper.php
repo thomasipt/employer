075d70dc0f1f8dfec62f764c502d9fde111a184e
Tino Oasis Laptop <falentinodjoka2801@gmail.com>
2024-02-28 23:13:23 +0700

    Helper baru: CustomNumber

---
+++ app/Helpers/CustomNumber_helper.php
@@ -0,0 +1,10 @@
+<?php
+    function convertToInt($formattedNumber){
+        $replacement    =   preg_replace('/,/', '', $formattedNumber);
+        return $replacement;
+    }
+    function convertToString($number){
+        $replacement    =   number_format($number, 0, ',');
+        return $replacement;
+    }
+?>
