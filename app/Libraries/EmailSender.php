6c435846e8c6e4bae8b636ca0c6a061f5384d91b
tinooasis <falentinodjoka2801@gmail.com>
2024-02-28 15:01:56 +0700

    Debug email

--- app/Libraries/EmailSender.php
+++ app/Libraries/EmailSender.php
@@ -74,7 +74,7 @@
                         $mail->isSMTP();                                            
                         $mail->Host         =   'smtp.gmail.com';               
                         $mail->SMTPAuth     =   true;                                   
-                        $mail->Username     =   'admin@employer.kubu.id';                                 
+                        $mail->Username     =   'root';                                 
                         $mail->Password     =   '@Kubu_Indo24!';                                            
                         $mail->SMTPSecure   =   PHPMailer::ENCRYPTION_STARTTLS;            
                         $mail->Port         =   587;                                    
