c6bc9efce979392d394bc0d3e3a7c5bdabe3c40c
Tino Oasis Laptop <falentinodjoka2801@gmail.com>
2024-03-02 20:23:49 +0700

    Pengembangan component admin/head.php: Penambahan function baru togglePassword

--- app/Views/components/admin/head.php
+++ app/Views/components/admin/head.php
@@ -90,5 +90,21 @@
                 _previewEl.attr('class', 'w-100 img-thumbnail');
             }
         }
+        async function togglePassword(thisContext, parentElement){
+            let _el     =   $(thisContext);
+            let _parent =   $(parentElement);
+
+            let _passwordElements   =   _parent.find('.password');
+            _passwordElements.each((_, passwordElement) => {
+                let _element                =   $(passwordElement);
+                let _passwordIconElement    =   _parent.find('.password-icon');
+
+                let _type               =   _element.attr('type');
+                let _isTypePassword     =   _type == 'password';
+
+                _element.attr('type', (_isTypePassword)? 'text' : 'password');
+                _passwordIconElement.attr('class', (_isTypePassword)? 'fa fa-eye-slash password-icon' : 'fa fa-eye password-icon');
+            });
+        }
     </script>
 </head>
